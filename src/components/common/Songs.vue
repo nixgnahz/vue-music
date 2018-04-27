735788f24043084182cdeb6ec38599f9905c60c0
nixgnahz <surezhangxin@163.com>
2018-04-27 14:44:47 +0800

    finish most layout

--- src/components/common/Songs.vue
+++ src/components/common/Songs.vue
@@ -32,26 +32,5 @@
 </script>
 
 <style>
-  .songs {
-    width: 90%;
-    margin: 1rem 5% 0px;
-  }
 
-  .songs li {
-    margin: 0.75rem 0px;
-  }
-
-  .songs li .title {
-    font-size: 0.8rem;
-    font-weight: 800;
-  }
-
-  .songs li .desc {
-    width: 90%;
-    overflow: hidden;
-    text-overflow:ellipsis;
-    white-space: nowrap;
-    color: #aaa;
-    font-size: 0.65rem;
-  }
 </style>
